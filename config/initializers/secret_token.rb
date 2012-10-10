26b0d4fdde7c515299874dbe4d17c4f992dfb560
Menisy <elmenisy@gmail.com>
2012-10-10 15:49:12 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Augment::Application.config.secret_token = 'e8062bcd1099f292ca64d748306a1f4e55214cf4ef7d0ce2f178c36c8536eaa5c668b0575769efc1ae5175bb7df224e73fd32dca83234f7e74d4e60a4958e23a'
